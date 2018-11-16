7280cc3a4c06f8838d9a6625a18af00ef46f2e2c
Lau <laujorpan@gmail.com>
2018-11-16 18:16:33 +0100

    Deploying my first angular app in heroku

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'prueba-webpage';
+  numero1 = 1;
+  numero2 = 2;
+  nombre = 'Laura';
+
+  marca:string  = 'BMW';
+  modelo:string = 'Serie 1';
+  matricula:string = 'NMK 2211';
+  precio: number = 100;
+
+  error: string;
+
+  isInit:boolean = false;
+
+  changeName (){
+    this.nombre=prompt("Introduce tu nombre")
+  }
+
+  changePrecio(){
+    this.error = undefined;
+    //this.precio = parseInt(prompt("Introduce el nuevo precio")); //Se queda con la parte entera
+    this.precio = Number(prompt("Introduce el nuevo precio")); //Mas eficiente, mantiene decimales
+    if (isNaN(this.precio)){
+      this.error = "Error en precio"
+      this.precio = 0;
+    }
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MicomponenteComponent } from './micomponente/micomponente.component';
+import { PersonaComponent } from './persona/persona.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MicomponenteComponent,
+    PersonaComponent
   ],
   imports: [
     BrowserModule,
